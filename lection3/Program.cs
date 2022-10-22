511e5b5df7cd7d6148588c2862135f4644adb225
AnastasiaBuglakova <bugla2005@mail.ru>
2022-10-22 21:27:06 +0300

    First init Lection3

---
+++ lection3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Вид 1 - не возвращают и не принимают
+/*void Method1()
+{
+    Console.WriteLine("Автор: Анастасия Буглакова");
+}
+Method1();*/
+
+// Вид 2  - не возвращают, но принимают
+/*void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Тext of message");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+       Console.WriteLine(msg);
+       i++; 
+    }
+}
+Method21("bla, bla", 3);
+// а можно указать и явно:
+Method21(msg: "bla, bla, bla", count: 4);
+// и не обязательно по порядку 
+Method21(count: 1, msg: "bla");*/
+
+// Вид 3  - возвращают, но не принимают
+// Зацикливание програмы остановить => ctrl + c
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
