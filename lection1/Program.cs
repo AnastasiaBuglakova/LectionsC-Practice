6d64bb8605a6b83f5933fcfa898b23e97d79bd1d
AnastasiaBuglakova <bugla2005@mail.ru>
2022-10-25 10:23:18 +0300

    Triangle is done

--- lection1/Program.cs
+++ lection1/Program.cs
@@ -33,7 +33,7 @@ else
 Console.WriteLine("Привет, " + userName);*/
 
 // Example006
-int a = 1;
+/*int a = 1;
 int b = 9;
 int c = 0;
 int d = 3;
@@ -45,4 +45,42 @@ if(max < c) max = c;
 if(max < d) max = d;
 if(max < e) max = e;
 if(max < f) max = f;
-Console.Write("max = " + max);
+Console.Write("max = " + max);*/
+
+// Example007
+Console.Clear();
+//Console.SetCursorPosition(10, 0);
+//Console.WriteLine("+");
+int xa = 40, ya = 1, 
+    xb = 1, yb = 30, 
+    xc = 80, yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+int x = xa, y = ya;
+int count = 0;
+while(count < 5000)
+{
+    int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+    if(what == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+    if(what == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
